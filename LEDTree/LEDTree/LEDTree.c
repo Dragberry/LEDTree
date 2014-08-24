6bf3b85cc5dfaa82121445fa2b480766e399c7c9
MaksimDragun <MaksimDragun@live.com>
2014-08-24 20:44:16 +0300

    #2 Init AVR project

---
+++ LEDTree/LEDTree/LEDTree.c
@@ -0,0 +1,17 @@
+/*
+ * LEDTree.c
+ *
+ * Created: 24.08.2014 20:43:08
+ *  Author: Maksim Dragun
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
